e8584a1856dff7a975b54fdd2db95ccf4e3b0e90
oussamaaatifi1 <nisowpro@gmail.com>
2024-04-18 17:28:10 +0100

    ajouter formulaire memebers et aussi create class javabeans pour members

--- src/main/java/biblio/metier/Membre.java
+++ src/main/java/biblio/metier/Membre.java
@@ -1,5 +1,55 @@
 package biblio.metier;
 
+
 public class Membre {
+   private int id_memeber;
+   private String First_name;
+   private String Last_name;
+   private int Num_tele;
+   private String email;
+
+
+   public Membre(int id_memeber, String first_name, String last_name, int num_tele, String email) {
+	super();
+	First_name = first_name;
+	Last_name = last_name;
+	Num_tele = num_tele;
+	this.email = email;
+	}
+
+	public String getFirst_name() {
+	return First_name;
+	}
+
+	public void setFirst_name(String first_name) {
+	First_name = first_name;
+	}
+
+	public String getLast_name() {
+	return Last_name;
+	}
+
+	public void setLast_name(String last_name) {
+	Last_name = last_name;
+	}
+
+	public int getNum_tele() {
+	return Num_tele;
+	}
+
+	public void setNum_tele(int num_tele) {
+	Num_tele = num_tele;
+	}
+
+	public String getEmail() {
+	return email;
+	}
+
+	public void setEmail(String email) {
+	this.email = email;
+	}
 
 }
+
+
+
---
+++ src/main/webapp/js/member.js
@@ -0,0 +1,22 @@
+    function openAddMemberPopup() {
+        document.getElementById("addMemberPopup").style.display = "block";
+        document.body.style.overflow = "hidden"; /* Disable scrolling on body when popup is open */
+    }
+
+    function closeAddMemberPopup() {
+        document.getElementById("addMemberPopup").style.display = "none";
+        document.body.style.overflow = "auto"; /* Enable scrolling on body when popup is closed */
+    }
+
+
+    function openUpdateMemberPopup() {
+        document.getElementById("addMemberPopup").style.display = "block";
+        document.body.style.overflow = "hidden"; /* Disable scrolling on body when popup is open */
+    }
+
+    function closeUpdateMemberPopup() {
+        document.getElementById("addMemberPopup").style.display = "none";
+        document.body.style.overflow = "auto"; /* Enable scrolling on body when popup is closed */
+    }
+
+ 
