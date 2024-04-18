faf4e89a2e1b40e17645710d8ab56b8b1d059770
RdFatimazahra <fatimaezahraradi@gmail.com>
2024-04-18 11:28:26 +0100

    Etablir la connexion JDBC 'Lier Eclipse ac postgres'

--- src/main/java/biblio/dao/Connect.java
+++ src/main/java/biblio/dao/Connect.java
@@ -1,5 +1,27 @@
 package biblio.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Connect {
 
+    public static void main(String[] args) {
+        Connection con = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/GestionBiblio", "postgres", "admin");
+            System.out.println("Connexion établie avec succès.");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Erreur lors de la connexion : " + e.getMessage());
+        } finally {
+            try {
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException e) {
+                System.out.println("Erreur lors de la fermeture de la connexion : " + e.getMessage());
+            }
+        }
+    }
 }
